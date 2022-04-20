bc55a6c920feb902048724c7b3041699982df7e5
vicctor1999 <victorperezalfayate@gmail.com>
2022-04-20 17:57:07 +0200

    commit victor 1

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express()
+const port = 8080
+
+const cors = require('cors');
+var corsOptions={
+    origin:'*',
+    optionsSuccessStatus: 200
+}
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors(corsOptions));
+
+
+//app.use('/api/routes',require('./routes/routes'));
+
+
+app.listen(port, () => {
+    console.log('Ejecutando...')    
+});
---
+++ backend/routes/routes.js
@@ -0,0 +1,9 @@
+/*const app = require('express');
+const router=require('express').Router();
+
+
+
+
+router.get('/', async (req,res)=> {
+    res.json("hola mundo");
+});*/
